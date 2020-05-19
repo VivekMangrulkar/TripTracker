21ce64ef125e99b14131991c5e2a707d2e2fbb8a
VivekMangrulkar <vivekmangrulkar@hotmail.com>
2020-05-20 09:22:52 +1000

    First Module

---
+++ TripTracker.BackService/Controllers/TripsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TripTracker.BackService.Models;
+
+namespace TripTracker.BackService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TripsController : Controller
+    {
+        private Repository _repository;
+
+        public TripsController(Repository repository)
+        {
+            _repository = repository;
+        }             
+
+        [HttpGet]
+        public IEnumerable<Trip> Get()
+        {
+            return _repository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public Trip Get(int id)
+        {
+            return _repository.Get(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Trip value)
+        {
+            _repository.AddTrip(value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put([FromBody]Trip value)
+        {
+            _repository.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _repository.Remove(id);
+        }
+    }
+}
---
+++ TripTracker.BackService/Models/Repository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripTracker.BackService.Models
+{
+    public class Repository
+    {
+        private List<Trip> MyTrips = new List<Trip>
+        {
+            new Trip
+            {
+                Id = 1,
+                Name = "India",
+                StartDarte = new DateTime(2020,03,12),
+                EndDate = new DateTime(2020, 03, 20)
+            },
+            new Trip
+            {
+                Id = 2,
+                Name = "Sydney",
+                StartDarte = new DateTime(2020,06,15),
+                EndDate = new DateTime(2020, 06, 20)
+            },
+             new Trip
+            {
+                Id = 2,
+                Name = "Melbourne",
+                StartDarte = new DateTime(2020,07,03),
+                EndDate = new DateTime(2020, 07, 05)
+            }
+        };
+
+        public List<Trip> Get()
+        {
+            return MyTrips;
+        }
+
+        public Trip Get(int id)
+        {
+            return MyTrips.First(t => t.Id == id);
+        }
+
+        public void AddTrip(Trip newTrip)
+        {
+            MyTrips.Add(newTrip);
+        }
+
+        public void Update(Trip tripToUpdate)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.Id == tripToUpdate.Id));
+            MyTrips.Add(tripToUpdate);
+        }
+
+        public void Remove(int id)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.Id == id));
+        }
+    }
+}
